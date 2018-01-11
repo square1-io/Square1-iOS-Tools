52ac47019aecf026262cf75f572f17dd91be8204
Roberto Pastor <wedge.hero@gmail.com>
2018-01-11 12:12:59 +0000

    Initial Commit

---
+++ Square1-iOS-Tools/Square1_iOS_Tools.h
@@ -0,0 +1,19 @@
+//
+//  Square1_iOS_Tools.h
+//  Square1-iOS-Tools
+//
+//  Created by Roberto Pastor Ortiz on 11/1/18.
+//  Copyright © 2018 Square1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Square1_iOS_Tools.
+FOUNDATION_EXPORT double Square1_iOS_ToolsVersionNumber;
+
+//! Project version string for Square1_iOS_Tools.
+FOUNDATION_EXPORT const unsigned char Square1_iOS_ToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Square1_iOS_Tools/PublicHeader.h>
+
+
