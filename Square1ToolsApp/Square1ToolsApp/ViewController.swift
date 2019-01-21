40c40ffa183dd4d5e1bbebc23e6c13fcaf2d2a76
Ginés Navarro Fernández <gines@Giness-MacBook-Pro.local>
2019-01-21 13:11:12 +0100

    Add Square1ToolsApp

---
+++ Square1ToolsApp/Square1ToolsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Square1ToolsApp
+//
+//  Created by Ginés Navarro Fernández on 21/01/2019.
+//  Copyright © 2019 Ginés Navarro Fernández. All rights reserved.
+//
+
+import UIKit
+import Square1Tools
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
