fb1771618540f73773379b0b5cb49204aa6a6ec2
Ginés Navarro Fernández <gines@Giness-MacBook-Pro.local>
2019-01-23 12:42:21 +0100

    Example TextViews

--- Square1ToolsApp/Square1ToolsApp/MainTableViewController.swift
+++ Square1ToolsApp/Square1ToolsApp/MainTableViewController.swift
@@ -13,7 +13,8 @@ class MainTableViewController: UITableViewController {
   private lazy var items: [String] = {
     return ["Keyboard",
             "Storyboard",
-            "Labels"]
+            "Labels",
+            "TextViews"]
   }()
   
   override func viewDidLoad() {
@@ -32,6 +33,10 @@ class MainTableViewController: UITableViewController {
   
   func goToLabels() {
     performSegue(withIdentifier: "toLabels", sender: nil)
+  }
+  
+  func goToTextViews() {
+    performSegue(withIdentifier: "toTextViews", sender: nil)
   }
   
   // MARK: - TableView DataSource.
@@ -62,6 +67,8 @@ class MainTableViewController: UITableViewController {
       goToStoryboard()
     case 2:
       goToLabels()
+    case 3:
+      goToTextViews()
     default:
       break
     }
---
+++ Square1ToolsApp/Square1ToolsApp/TextViews/TextViewsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TextViewsViewController.swift
+//  Square1ToolsApp
+//
+//  Created by Ginés Navarro Fernández on 23/01/2019.
+//  Copyright © 2019 Ginés Navarro Fernández. All rights reserved.
+//
+
+import UIKit
+
+class TextViewsViewController: UIViewController {
+  
+}
