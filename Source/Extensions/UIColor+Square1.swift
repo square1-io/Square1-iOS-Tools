b76309c6cd54c4af331a17b89ad79d460f7385b2
Ginés Navarro Fernández <gines@Giness-MacBook-Pro.local>
2019-01-24 10:56:15 +0100

    Add alpha

--- Source/Extensions/UIColor+Square1.swift
+++ Source/Extensions/UIColor+Square1.swift
@@ -29,12 +29,13 @@ public extension UIColor {
   ///   - red: Red value.
   ///   - green: Green value.
   ///   - blue: Blue value.
-  convenience init(red: Int, green: Int, blue: Int) {
+  ///   - alpha: Alpha value. Default 1.0
+  convenience init(red: Int, green: Int, blue: Int, alpha: CGFloat = 1.0) {
     assert(red >= 0 && red <= 255, "Invalid red component")
     assert(green >= 0 && green <= 255, "Invalid green component")
     assert(blue >= 0 && blue <= 255, "Invalid blue component")
     
-    self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: alpha)
   }
   
   /// Convenience init to create UIColor with RGB joint Int value.
